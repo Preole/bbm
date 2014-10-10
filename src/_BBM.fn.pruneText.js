ce3d5d8f5d4d53aa944103785e959833019f8e9d
preole <jca142@hotmail.com>
2014-10-10 11:27:55 +0800

    Finished Combine Text (pruneText) Traversal.

--- src/_BBM.fn.pruneText.js
+++ src/_BBM.fn.pruneText.js
@@ -1,16 +1,45 @@
 (function (){
 "use strict";
 
-var BBM = require("./BBM.js");
+var BBM = require("./BBM.js"),
+DUMMY = BBM("TODODummyNodeCreation"),
+ENUM = BBM.ENUM;
 
 
 
+function _pruneTextWork(newNode, node)
+{
+ var prev = newNode.last() || DUMMY;
+ if (prev.type() === ENUM.TEXT && node.type() === ENUM.TEXT)
+ {
+  prev.val(prev.val() + node.val());
+ }
+ else
+ {
+  newNode.append(node);
+ }
+}
 
+function _prunable(node, index, sibs)
+{
+ var next = sibs[index + 1] || DUMMY;
+ return node.type() === ENUM.TEXT
+  && next.type() === ENUM.TEXT
+  && next !== DUMMY;
+}
+
+function _pruneText(node)
+{
+ if (node.children().some(_prunable))
+ {
+  node.reduceChild(_pruneTextWork);
+ }
+}
 
 //Combines consecutive text nodes within the tree into one node.
 function pruneText()
 {
- 
+ return this.eachPre(_pruneText, this);
 }
 
 BBM.prototype.pruneText = pruneText;
