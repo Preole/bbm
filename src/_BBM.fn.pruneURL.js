401749e23aa813a11198d93ec1662ac70a5047d8
preole <jca142@hotmail.com>
2014-10-10 11:48:13 +0800

    Implement URL Analysis Pass

--- src/_BBM.fn.pruneURL.js
+++ src/_BBM.fn.pruneURL.js
@@ -1,20 +1,43 @@
 (function (){
 "use strict";
 
-var BBM = require("./BBM.js");
+var BBM = require("./BBM.js"),
+ENUM = BBM.ENUM,
+LINKS = [AST.LINK_EXT, AST.LINK_INT, AST.LINK_WIKI],
+SYMTABLE = {};
 
 
-
-
-function _pruneURL(node, )
+function _pruneHREF(node)
 {
-
+ node.attr("href", BBM.get(this, node.attr("href")) || node.attr("href"));
+ if (node.size() === 0)
+ {
+  node.append(node.attr("href"));
+ }
 }
 
-//Resolves reference links.
+function _pruneIMG(node)
+{
+ node.attr("src", BBM.get(this, node.attr("src")) || node.attr("src"));
+}
+
+function _pruneURL(node)
+{
+ var nType = node.type();
+ if (nType === ENUM.IMG)
+ {
+  _pruneIMG(node, this);
+ }
+ else if (LINKS.indexOf(nType) > -1)
+ {
+  _pruneHREF(node, this);
+ }
+}
+
 function pruneURL()
 {
- return this.eachPost(_pruneURL, this);
+ var refTable = BBM.isObject(this.refTable) ? this.refTable : SYMTABLE;
+ return this.eachPost(_pruneURL, refTable);
 }
 
 BBM.prototype.pruneURL = pruneURL;
