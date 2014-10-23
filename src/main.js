45f3bedd23b65b0bfa1de6239ddfb0f1c773cf39
preole <jca142@hotmail.com>
2014-10-23 16:15:49 +0800

    Add UMD export template

---
+++ src/main.js
@@ -0,0 +1,32 @@
+(function (root, factory){
+"use strict";
+
+if (typeof define === "function" && define.amd)
+{
+ define([], factory);
+}
+else if (typeof module === "object" && typeof module.exports === "object")
+{
+ module.exports = factory();
+}
+else if (typeof module === "object" && typeof exports === "object")
+{
+ exports.BBM = factory(); 
+}
+else
+{
+ root.BBM = factory(); 
+}
+
+
+
+}(this, function (){
+"use strict";
+
+
+//@StringReplace
+
+return BBM;
+}));
+
+
