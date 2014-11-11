59bec8d72086e6e8852849619ee4c448c931218c
preole <jca142@hotmail.com>
2014-11-10 16:31:59 -0800

    Better Testing Script
    - Add rudimentary line labels "+" and "-".
    -

--- s-test.js
+++ s-test.js
@@ -2,9 +2,9 @@
 
 var BBM = require("./dist/bbm.js");
 var fs = require("fs");
-var fsOptR = {encoding: "utf-8"};
 var glob = require("glob");
 var diff = require("diff");
+var fsOptR = {encoding: "utf-8"};
 
 var srcList = glob.sync("./tests/*.txt").sort();
 var destList = glob.sync("./tests/*.html").sort();
@@ -12,10 +12,24 @@ var destList = glob.sync("./tests/*.html").sort();
 srcList.forEach(function (srcFile, index){
  var testCase = BBM.parse(fs.readFileSync(srcFile, fsOptR)).toHTML().trim();
  var expected = fs.readFileSync(destList[index], fsOptR).trim();
+ var buffer = "";
  
  //TODO: More elaborate diffing.
  diff.diffLines(expected, testCase).forEach(function (changeObj){
-  console.log(changeObj); 
+  if (changeObj.added)
+  {
+   buffer += "+ " + changeObj.value + "";
+  }
+  else if (changeObj.removed)
+  {
+   buffer += "- " + changeObj.value + "";
+  }
  });
+ 
+ if (buffer)
+ {
+  console.log("Test case " + srcFile);
+  console.log(buffer);
+ }
 });
 
